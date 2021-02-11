ad528080829aca856d86faa5ff9f42fa84abae35
kcaneer <kcaneer@gmail.com>
2021-02-11 09:30:09 -0500

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
+var cors = require("cors");
 var app = express();
+
+app.use(cors());
 
 app.get("/api/details", function (req, res) {
   var sql = require("mssql");
